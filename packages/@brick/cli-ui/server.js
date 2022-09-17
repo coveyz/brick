b1b2f7ec88fe4111ac1d67c878df48e52966eb2c
Saturday <zhangkairong123@qq.com>
2022-09-17 16:49:49 +0800

    feat: 创建 ui工程

---
+++ packages/@brick/cli-ui/server.js
@@ -0,0 +1,4 @@
+import portfinder from 'portfinder';
+import server from './graphql-server.js';
+
+export { portfinder, server };
--- packages/@brick/cli/lib/ui.js
+++ packages/@brick/cli/lib/ui.js
@@ -1,8 +1,11 @@
 import { openBrowser } from '@brick/cli-shared-utils';
+import { portfinder, server } from '@brick/cli-ui/server.js';
 
 function ui(options = {}, context = process.cwd) {
 	console.log('ui=>');
-	console.log('test-sharedUtils-openBrowser', openBrowser);
+	// console.log('test-sharedUtils-openBrowser', openBrowser);
+	// console.log('test-sharedUtils-portfinder', portfinder);
+	// console.log('test-sharedUtils-server', server);
 }
 
 export default (...args) => {
